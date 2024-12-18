cfbf486f776234eed2a17d61e6c9ce6cf8d6e57d
DianisMartinez <dianagomez192002@gmail.com>
2024-12-18 23:39:42 +0000

    Casi terminado

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User , Planet,Character,Vehicle
 #from models import Person
 
 app = Flask(__name__)
@@ -31,19 +31,68 @@ setup_admin(app)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_all_user():
+    all_users = User.query.all()  
+    result = [user.serialize() for user in all_users]  
+    return jsonify(result), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    
 
-    return jsonify(response_body), 200
+
+@app.route('/people', methods=['GET'])
+def get_all_people():
+    all_people = Character.query.all()
+    result = [people.serialize() for people in all_people]  
+    return jsonify(result) ,200
+
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+    all_planets = Planet.query.all()
+    result = [user.serialize() for user in all_planets]  
+    return jsonify(result) ,200
+
+
+@app.route('/vehicle', methods=['GET'])
+def get_vehicle():
+    get_vehicle = Vehicle.query.all()
+    result = [user.serialize() for user in get_vehicle]  
+    return jsonify(result) ,200
+    
+
+@app.route('/planet/<int:id>', methods=['GET'])
+def get_one_planet(id):
+     one_planet = Planet.query.get(id)
+     if one_planet is None: 
+      return "PLANETA NO SE ENCUENTRA", 400
+     result = one_planet.serialize()
+     return jsonify(result),200
+
+@app.route('/vehicle/<int:id>', methods=['GET'])
+def get_one_vehicle(id):
+     one_vehicle = Vehicle.query.get(id)
+     if one_vehicle is None: 
+      return "VEHICLE NO SE ENCUENTRA", 400
+     result = one_vehicle.serialize()
+     return jsonify(result),200
+
+@app.route('/character/<int:id>', methods=['GET'])
+def get_one_character(id):
+    one_character = Character.query.get(id)
+    if one_character is None: 
+        return "CHARACTER NO ENCONTRADO", 400
+    result = one_character.serialize() 
+    return jsonify(result), 200
+
+
+
+
+    
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,163 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), unique=False, nullable=False)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+#     def __repr__(self):
+#         return '<User %r>' % self.username
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "email": self.email,
+#             # do not serialize the password, its a security breach
+#         }
+     
+class User(db.Model):
+    __tablename__ = 'user'
+   
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(250), unique=True ,nullable=False)
+    first_name = db.Column(db.String(250), nullable=False)
+    last_name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    fecha_suscripcion = db.Column(db.DateTime(timezone=True), nullable=False)
+
+    def serialize(self):
+     return {
+        "id": self.id,
+        "user_name": self.user_name,
+        "first_name": self.first_name,
+        "last name":self.last_name,
+        "email": self.email,
+        "password": self.password,
+        "fecha_suscripcion": self.fecha_suscripcion
+    }
+      
+
+class Planet(db.Model):
+    __tablename__= 'planet'
+
+    id = db.Column(db.Integer, primary_key=True)
+    planet_name = db.Column(db.String(250),nullable=False)
+    diameter = db.Column(db.String(250), nullable=False)
+    population = db.Column(db.String(250), nullable=False)
+    climate = db.Column(db.String(250), nullable=False)
+
+    def serialize(self):
+       return {
+           "id": self.id,
+           "planet_name": self.planet_name,
+           "diameter": self.diameter,
+           "population":self.population,
+           "climate": self.climate,
+        
+    }
+    
+  
+class Character(db.Model):
+    __tablename__ = 'character'
+
+    id = db.Column(db.Integer, primary_key=True)
+    character_name = db.Column(db.String(250),nullable=False)
+    height = db.Column(db.Integer,nullable=False)
+    hair_color = db.Column(db.String(250),nullable=False)
+    eye_color = db.Column(db.String(250),nullable=False)
 
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            'id': self.id,
+            'character_name ': self.character_name,
+            'height': self.height,
+            'hair_color': self.hair_color,
+            'eye_color': self.eye_color
         }
+    
+ 
+
+class Vehicle(db.Model):
+    __tablename__ = 'vehicle'
+
+    id = db.Column(db.Integer, primary_key=True)
+    vehicle_name = db.Column(db.String(250),nullable=False)
+    cost_in_credits = db.Column(db.String(250),nullable=False)
+    model = db.Column(db.String(250),nullable=False)
+    passengers = db.Column(db.String(250),nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'vehicle_name': self.vehicle_name,
+            'model': self.model,
+            'cost_in_credits':self.cost_in_credits,
+            'passengers': self.passengers
+        }
+   
+
+
+class Create_Planet(db.Model):
+    __tablename__ = 'create_planet'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    planet_create_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=False)
+    planet = db.relationship('Planet')
+    user = db.relationship('User')
+    
+
+class Create_Vehicle(db.Model):
+    __tablename__ = 'create_vehicle'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    vehicle_create_id = db.Column(db.Integer, db.ForeignKey('vehicle.id'), nullable=False)
+    vehicle= db.relationship('Vehicle')
+    user = db.relationship('User')
+    
+class Create_Character(db.Model):
+    __tablename__ = 'create_character'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    character_create_id = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=False)
+    character = db.relationship('Character')
+    user = db.relationship('User')
+    
+
+class Favorito_Planet(db.Model):
+    __tablename__ = 'favorito_planet'
+
+    id = db.Column(db.Integer, primary_key=True)
+    planet_favorito_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    planet = db.relationship('Planet')
+    user = db.relationship('User')
+    
+    
+class Favorito_Character(db.Model):
+    __tablename__ = 'favorito_character'
+
+    id = db.Column(db.Integer, primary_key=True)
+    character_favorito_id = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    character = db.relationship('Character')
+    user = db.relationship('User')
+    
+class Favorito_Vehicle(db.Model):
+    __tablename__ = 'favorito_vehicle'
+
+    id = db.Column(db.Integer, primary_key=True)
+    vehicle_favorito_id = db.Column(db.Integer, db.ForeignKey('vehicle.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    vehicle = db.relationship('Vehicle')
+    user = db.relationship('User')
+    
+
